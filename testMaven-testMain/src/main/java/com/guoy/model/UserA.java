1e9ebd6c23ddb65091fd0feacfcdf8a77e62340c
guoyeb <guoyeb@enn.cn>
2018-01-11 17:21:14 +0800

    批量修改文件

---
+++ testMaven-testMain/src/main/java/com/guoy/model/UserA.java
@@ -0,0 +1,36 @@
+package com.guoy.model;
+
+public class UserA {
+    /****/
+    private String userid;
+
+    /****/
+    private Integer name;
+
+    /****/
+    private String starttime;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(Integer name) {
+        this.name = name;
+    }
+
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(String starttime) {
+        this.starttime = starttime;
+    }
+}
